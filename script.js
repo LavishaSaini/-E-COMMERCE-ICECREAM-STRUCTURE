a7b1e9d997e31890f9f8ea678493d18bd5d1a7ea
Lavisha <167676607+LavishaSaini@users.noreply.github.com>
2025-01-13 16:41:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+document.querySelector('.more').addEventListener('click',()=>{
+    prompt('Purchased successfully');
+})
+
+function buyDone(){
+    alert('Purchased successfully');
+    
+}
+
+document.querySelector('.submit').addEventListener('click',()=>{
+    prompt('Congratulations.Your record are sucessfully submit');
+})
+
+function buysubmit(){
+    alert(' Congratulations.Your record are sucessfully submit');
+}
+document.querySelector('.cart').addEventListener('click',()=>{
+  prompt('ADD to cart succesfully');
+})
+
+function buyAdd(){
+  alert(' Add to cart succesfully');
+}
+
